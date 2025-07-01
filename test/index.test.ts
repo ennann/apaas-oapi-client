46cbd289b44beaa0fcf0e19bd49a854267809d7e
ennann <hi@yizhe.me>
2025-07-01 17:24:06 +0800

    chore(release): publish new version

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,13 +2,20 @@ import { apaas } from '../src/index';
 
 describe('aPaaS SDK', () => {
     it('should get access token and query records with iterator', async () => {
+        const clientId = process.env.CLIENT_APP_ID || 'xxx';
+        const clientSecret = process.env.CLIENT_APP_SECRET || 'xxx';
+
+        if (clientId === 'xxx' || clientSecret === 'xxx') {
+            console.warn('No valid clientId or clientSecret provided, skipping test.');
+            return;
+        }
+
         const client = new apaas.Client({
-            clientId: 'xxx',
-            clientSecret: 'xxx',
+            clientId,
+            clientSecret,
             namespace: 'app_146999_store__c'
         });
 
         await client.init();
-
     });
 });
